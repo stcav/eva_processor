bd88f15d8c0d553f6626a3c3cbe7dbd76d23eaf0
Johaned <johan.tique@codescrum.com>
2014-06-01 19:54:26 -0500

    add JMXProcessAttributes Class in order to encapsulate the attributes implied in media processing

---
+++ src/org/university/stcav/evaprocessor/model/JMXProcessAttributes.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.university.stcav.evaprocessor.model;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author johaned
+ */
+public class JMXProcessAttributes {
+    private String vCodec;
+    private String aCodec;
+    private String resolution;
+    private int duration;
+    private int processDuration;
+    private int bitrate;
+    HashMap <String, Object> attributes;
+
+    public JMXProcessAttributes(String vCodec, String aCodec, String resolution, int duration, int processDuration, int bitrate) {
+        this.vCodec = vCodec;
+        this.aCodec = aCodec;
+        this.resolution = resolution;
+        this.duration = duration;
+        this.processDuration = processDuration;
+        this.bitrate = bitrate;
+        attributes = new HashMap<String, Object>();
+    }
+    
+    public HashMap<String, Object> getAttributes(){
+        attributes.clear();
+        attributes.put("VCodec", this.vCodec);
+        attributes.put("ACodec", this.aCodec);
+        attributes.put("Resolution", this.resolution);
+        attributes.put("Duration", this.duration);
+        attributes.put("ProcessDuration", this.processDuration);
+        attributes.put("Bitrate", this.bitrate);
+        return attributes;
+    }
+
+    public String getvCodec() {
+        return vCodec;
+    }
+
+    public String getaCodec() {
+        return aCodec;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getProcessDuration() {
+        return processDuration;
+    }
+
+    public int getBitrate() {
+        return bitrate;
+    }
+
+    public void setvCodec(String vCodec) {
+        this.vCodec = vCodec;
+    }
+
+    public void setaCodec(String aCodec) {
+        this.aCodec = aCodec;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setProcessDuration(int processDuration) {
+        this.processDuration = processDuration;
+    }
+
+    public void setBitrate(int bitrate) {
+        this.bitrate = bitrate;
+    }
+    
+}
